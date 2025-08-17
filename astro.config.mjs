8fab0475607aff301e7322626a4a5a1b69c1f5ee
bakabo2st <spacecake420@gmail.com>
2025-08-17 18:03:22 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://bakabo2st.github.io', // 後で必ず自分のユーザー名に！
+  // project page運用にするなら base: '/<repo-name>' を追加
+});
